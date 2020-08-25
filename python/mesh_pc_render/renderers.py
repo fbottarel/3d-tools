34eb726026e9a6653036fae602b0ae6cb67e06e2
fbottarel <fabrizio.bottarel@iit.it>
2020-08-25 19:26:40 +0200

    added rendering using pyrender and EGL

--- python/mesh_pc_render/renderers.py
+++ python/mesh_pc_render/renderers.py
@@ -1,5 +1,6 @@
 import trimesh
 import numpy as np
+import pyrender
 
 def renderMeshAll(mesh, cam_viewpoint, number_sampled_points, z_towards_mesh=True):
     """
@@ -53,7 +54,6 @@ def renderMeshAll(mesh, cam_viewpoint, number_sampled_points, z_towards_mesh=Tru
     points_camera_frame = points_camera_frame_homogeneous[:,0:3]
 
     return points, points_camera_frame
-
 
 def renderMeshBackFaceCull(mesh, cam_viewpoint, number_sampled_points, z_towards_mesh=True):
     """
@@ -144,6 +144,122 @@ def renderMeshBackFaceCull(mesh, cam_viewpoint, number_sampled_points, z_towards
 
     return points, points_camera_frame
 
+def renderMeshEGL(mesh, cam_viewpoint, number_sampled_points, z_towards_mesh=True):
+    """
+    Renders a number of points on a mesh from a viewpoint, considering occlusion.
+    Faces not in sight are detected by casting rays from a camera viewpoint.
+
+    Parameters
+    ----------
+    mesh: trimesh.Mesh
+        Mesh to sample
+
+    cam_viewpoint: (3,) float
+        Point from which to observe the mesh
+
+    number_sampled_points: int
+        Number of points to sample on the mesh
+
+    Returns
+    ----------
+    points: (n, 3) float
+        Points sampled on the visible portion of the mesh, in the original mesh reference frame
+
+    points_camera_frame: (n, 3) float
+        Points sampled on the visible portion of the mesh, in the camera reference frame
+
+    """
+
+    import os
+    os.environ["PYOPENGL_PLATFORM"] = "egl"
+    mesh = pyrender.Mesh.from_trimesh(mesh)
+
+    scene = pyrender.Scene()
+    scene.add(mesh)
+
+    # Set up the camera and depth of field
+    fov = np.pi/3.0
+    height = 480
+    width = 640
+    fx = fy = width / 2.0 / np.tan(fov / 2.0)
+    cx = width / 2.0
+    cy = height / 2.0
+    # camera = pyrender.PerspectiveCamera(yfov=fov)
+    camera = pyrender.IntrinsicsCamera(fx=fx, fy=fy, cx=cx, cy=cy)
+
+    # z axis is the ray from the mesh center to the camera axis center
+    # This inversion is necessary because the camera reference system uses the cinematographic
+    # convention
+    # https://www.scratchapixel.com/lessons/mathematics-physics-for-computer-graphics/lookat-function
+
+    mesh_centroid = mesh.centroid
+    if z_towards_mesh:
+        camera_z_axis = (cam_viewpoint - mesh_centroid) / np.linalg.norm(mesh.centroid - cam_viewpoint)
+    else:
+        camera_z_axis = (mesh_centroid - cam_viewpoint) / np.linalg.norm(mesh.centroid - cam_viewpoint)
+
+    # Obtain the camera transformation as a 4x4 and add it
+
+    camera_transform_4x4 = trimesh.geometry.align_vectors(np.array([0,0,1], dtype=np.float32), camera_z_axis)
+    camera_transform_4x4[0:3,3] = cam_viewpoint
+    scene.add(camera, pose=camera_transform_4x4)
+
+    # Set up the light -- a single spot light in the same spot as the camera
+    light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
+                                innerConeAngle=np.pi/16.0)
+    scene.add(light, pose=camera_transform_4x4)
+
+    # Render the scene
+    r = pyrender.OffscreenRenderer(width, height)
+    color, depth = r.render(scene)
+
+    # Show the images
+    # import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.subplot(1,2,1)
+    # plt.axis('off')
+    # plt.imshow(color)
+    # plt.subplot(1,2,2)
+    # plt.axis('off')
+    # plt.imshow(depth, cmap=plt.cm.gray_r)
+    # plt.show()
+
+    # Transform to point cloud
+    # fy = fx = 0.5 / np.tan(fov * 0.5)
+    # height = depth.shape[0]
+    # width = depth.shape[1]
+    # mask = np.where(depth > 0)
+    # x = mask[1]
+    # y = mask[0]
+    # normalized_x = (x.astype(np.float32) - width * 0.5) / width
+    # normalized_y = (y.astype(np.float32) - height * 0.5) / height
+    # world_x = normalized_x * depth[y, x] / fx
+    # world_y = normalized_y * depth[y, x] / fy
+    # world_z = depth[y, x]
+
+    # points = np.vstack((world_x, world_y, world_z))
+
+    mask = np.where(depth > 0)
+    x = mask[1]
+    y = mask[0]
+    points_x = (x.astype(np.float32) - cx) / fx * depth[y,x]
+    points_y = (y.astype(np.float32) - cy) / fy * depth[y,x]
+    points_z = depth[y,x]
+
+    points_camera_frame = np.vstack((points_x, points_y, points_z)).T
+
+    # Downsample if necessary
+    if points_camera_frame.shape[0] > number_sampled_points:
+        choice = np.random.choice(points_camera_frame.shape[0], number_sampled_points, replace=False)
+        points_camera_frame = points_camera_frame[choice, :]
+
+    points_cam_homogeneous = np.hstack((points_camera_frame, np.ones((points_camera_frame.shape[0],1), dtype=np.float32)))
+
+    # Obtain point coordinates in the object-centric ref system
+    points_world_frame_homogeneous = np.transpose(np.matmul(np.linalg.inv(camera_transform_4x4), np.transpose(points_cam_homogeneous)))
+    points_world_frame = points_world_frame_homogeneous[:, 0:3]
+
+    return points_world_frame, points_camera_frame
 
 def renderMeshRayCast(mesh, cam_viewpoint, number_sampled_points, z_towards_mesh=True):
     """
