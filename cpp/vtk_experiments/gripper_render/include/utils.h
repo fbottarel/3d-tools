9392565c9273413bd918fd7172659484ab9df685
fbottarel <fabrizio.bottarel@iit.it>
2021-05-24 20:58:15 +0200

    added util functions'

---
+++ cpp/vtk_experiments/gripper_render/include/utils.h
@@ -0,0 +1,25 @@
+#include <fstream>
+
+#include <urdf_model/pose.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkBYUReader.h>
+#include <vtkOBJReader.h>
+#include <vtkPLYReader.h>
+#include <vtkPolyDataReader.h>
+#include <vtkSTLReader.h>
+#include <vtkXMLPolyDataReader.h>
+
+/// Check for file existence
+bool fileExists(const std::string filename);
+
+/// Pose to Eigen matrix
+Eigen::Matrix4f getHomogeneousTransform(const urdf::Pose &pose);
+
+/// Read mesh from file
+vtkSmartPointer<vtkPolyData> readPolyDataFromFile(std::string const& mesh_path);
