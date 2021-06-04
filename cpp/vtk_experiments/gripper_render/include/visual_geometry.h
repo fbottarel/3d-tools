6609e11dfc3849fd928ec6a8bb17f2b71d601d49
fbottarel <fabrizio.bottarel@iit.it>
2021-06-05 00:16:19 +0200

    added texture to visual geometry class

--- cpp/vtk_experiments/gripper_render/include/visual_geometry.h
+++ cpp/vtk_experiments/gripper_render/include/visual_geometry.h
@@ -16,6 +16,8 @@
 #include <vtkNamedColors.h>
 #include <vtkProperty.h>
 #include <vtkRenderer.h>
+#include <vtkPNGReader.h>
+#include <vtkTexture.h>
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
@@ -24,6 +26,8 @@
 #include <utils.h>
 
 #include <math.h>
+
+#include <filesystem>
 
 namespace mev
 {
@@ -35,15 +39,19 @@ namespace mev
 
         std::string mesh_path;
         std::string mesh_color;
+        std::string texture_path;
 
         Eigen::Matrix4f geometry_world_pose;
 
         vtkSmartPointer<vtkPolyData> geometry_polydata;
         vtkNew<vtkPolyDataMapper> geometry_mapper;
+        vtkSmartPointer<vtkTexture> geometry_texture;
+        vtkSmartPointer<vtkPNGReader> geometry_texture_reader;
         vtkNew<vtkActor> geometry_actor;
 
-        VisualGeometry(const std::string& path = "",
-                       const std::string& color = "SlateGray");
+        VisualGeometry(const std::string& model_path = "",
+                       const std::string& color = "SlateGray",
+                       const std::string& texture_path = "");
         bool setGeometryWorldPose(const Eigen::Matrix4f& geometry_world_pose);
         vtkSmartPointer<vtkActor> getGeometryActor();
         void addGeometryToRenderer(vtkSmartPointer<vtkRenderer> renderer);
@@ -56,7 +64,8 @@ namespace mev
         urdf::MeshSharedPtr urdf_geometry;
         std::string urdf_path;
 
-        URDFVisualGeometry(urdf::MeshSharedPtr urdf_geometry, const std::string& urdf_path = "");
+        URDFVisualGeometry(urdf::MeshSharedPtr urdf_geometry,
+                           const std::string& urdf_path = "");
     };
 
 }
--- cpp/vtk_experiments/gripper_render/src/visual_geometry.cpp
+++ cpp/vtk_experiments/gripper_render/src/visual_geometry.cpp
@@ -1,28 +1,40 @@
 #include "visual_geometry.h"
-#include <iostream>
 
 namespace mev
 {
-    VisualGeometry::VisualGeometry(const std::string& path,
-                                   const std::string& color)
-    : mesh_path {path},
-      mesh_color {color}
+    VisualGeometry::VisualGeometry(const std::string& model_path,
+                                   const std::string& color,
+                                   const std::string& texture_path)
+    : mesh_path {model_path},
+      mesh_color {color},
+      texture_path {texture_path}
     {
         // Standard VTK procedure to produce a polydata actor
         geometry_polydata = readPolyDataFromFile(mesh_path);
         geometry_mapper->SetInputData(geometry_polydata);
         geometry_actor->SetMapper(geometry_mapper);
         geometry_actor->SetOrigin(0,0,0);
-        vtkNew<vtkNamedColors> colors;
-        geometry_actor->GetProperty()->SetColor(colors->GetColor3d(mesh_color).GetData());
-
+        if (std::filesystem::exists(texture_path))
+        {
+            geometry_texture_reader = vtkSmartPointer<vtkPNGReader>::New();
+            geometry_texture = vtkSmartPointer<vtkTexture>::New();
+            geometry_texture_reader->SetFileName(texture_path.c_str());
+            geometry_texture_reader->Update();
+            geometry_texture->SetInputConnection(geometry_texture_reader->GetOutputPort());
+            geometry_actor->SetTexture(geometry_texture);
+        }
+        else
+        {
+            std::cout << "[WARNING] texture file " << texture_path << " does not exist" << std::endl;
+            vtkNew<vtkNamedColors> colors;
+            geometry_actor->GetProperty()->SetColor(colors->GetColor3d(mesh_color).GetData());
+        }
         // Geometry lays in the world origin ref frame by default
         geometry_world_pose = Eigen::Matrix4f::Identity();
     }
 
     bool VisualGeometry::setGeometryWorldPose(const Eigen::Matrix4f& geometry_world_pose)
     {
-        std::cout << "[DEBUG] setting " << std::endl << geometry_world_pose << std::endl;
         this->geometry_world_pose = geometry_world_pose;
 
         Eigen::Matrix3f rotation = geometry_world_pose.block<3,3>(0,0);
@@ -48,9 +60,6 @@ namespace mev
         geometry_actor->SetOrientation(rotation_euler_deg[1],
                                        rotation_euler_deg[2],
                                        rotation_euler_deg[0]);
-
-        std::cout << "geometry is at position " << geometry_actor->GetPosition()[0] << " " << geometry_actor->GetPosition()[1] << " " << geometry_actor->GetPosition()[2] << std::endl;
-
         return true;
     }
 
